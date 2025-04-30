f14d0f628fbdcd3a69e3ae2ebdc38308de398ed4
Rizka Indah Permata <indahka017@gmail.com>
2025-04-30 20:06:20 +0700

    Membuat tampilan list

--- app/src/main/java/com/rizkaindah0043/booksmine/ui/screen/MainScreen.kt
+++ app/src/main/java/com/rizkaindah0043/booksmine/ui/screen/MainScreen.kt
@@ -1,8 +1,14 @@
 package com.rizkaindah0043.booksmine.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +17,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rizkaindah0043.booksmine.R
+import com.rizkaindah0043.booksmine.model.Book
 import com.rizkaindah0043.booksmine.ui.theme.BooksMineTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,10 +49,45 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn (
+        modifier = modifier.fillMaxSize()
+    ){
+        items(data){
+            ListItem(book = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(book: Book) {
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = book.title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = book.writer ,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+        )
+        Text(
+            text = book.publishDate
+        )
+        Text(
+            text = book.synopsis,
+            maxLines = 4,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
 }
 
 @Preview(showBackground = true)
